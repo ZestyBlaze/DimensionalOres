cc17c7724005b7704a7de5bb0b3b7d2b62e2a72d
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2021-08-05 15:49:54 +0100

    Various Changes
    This makes changes to various aspects of the code
    - Moves all registries out of the main java class
    - Fixes texturing and loot tables problems
    - Improves generation in Nether and End

--- src/main/java/net/zestyblaze/dimensionalores/setup/NetherOres.java
+++ src/main/java/net/zestyblaze/dimensionalores/setup/NetherOres.java
@@ -1,10 +1,15 @@
 package net.zestyblaze.dimensionalores.setup;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.zestyblaze.dimensionalores.DimensionalOres;
 
 public class NetherOres {
     public static final Block NETHER_COAL = new Block(FabricBlockSettings.of(Material.STONE)
@@ -55,5 +60,29 @@ public class NetherOres {
             .sounds(BlockSoundGroup.NETHERRACK)
             .requiresTool()
     );
+
+    public static void registerBlocks() {
+        {
+            Registry.register(Registry.BLOCK, new Identifier(DimensionalOres.MOD_ID, "nether_coal"), NETHER_COAL);
+            Registry.register(Registry.BLOCK, new Identifier(DimensionalOres.MOD_ID, "nether_copper"), NETHER_COPPER);
+            Registry.register(Registry.BLOCK, new Identifier(DimensionalOres.MOD_ID, "nether_iron"), NETHER_IRON);
+            Registry.register(Registry.BLOCK, new Identifier(DimensionalOres.MOD_ID, "nether_lapis"), NETHER_LAPIS);
+            Registry.register(Registry.BLOCK, new Identifier(DimensionalOres.MOD_ID, "nether_redstone"), NETHER_REDSTONE);
+            Registry.register(Registry.BLOCK, new Identifier(DimensionalOres.MOD_ID, "nether_diamond"), NETHER_DIAMOND);
+            Registry.register(Registry.BLOCK, new Identifier(DimensionalOres.MOD_ID, "nether_emerald"), NETHER_EMERALD);
+        }
+    }
+
+    public static void registerBlockItems() {
+        {
+            Registry.register(Registry.ITEM, new Identifier(DimensionalOres.MOD_ID, "nether_coal"), new BlockItem(NETHER_COAL, new FabricItemSettings().group(DimensionalOres.MAIN_TAB)));
+            Registry.register(Registry.ITEM, new Identifier(DimensionalOres.MOD_ID, "nether_copper"), new BlockItem(NETHER_COPPER, new FabricItemSettings().group(DimensionalOres.MAIN_TAB)));
+            Registry.register(Registry.ITEM, new Identifier(DimensionalOres.MOD_ID, "nether_iron"), new BlockItem(NETHER_IRON, new FabricItemSettings().group(DimensionalOres.MAIN_TAB)));
+            Registry.register(Registry.ITEM, new Identifier(DimensionalOres.MOD_ID, "nether_lapis"), new BlockItem(NETHER_LAPIS, new FabricItemSettings().group(DimensionalOres.MAIN_TAB)));
+            Registry.register(Registry.ITEM, new Identifier(DimensionalOres.MOD_ID, "nether_redstone"), new BlockItem(NETHER_REDSTONE, new FabricItemSettings().group(DimensionalOres.MAIN_TAB)));
+            Registry.register(Registry.ITEM, new Identifier(DimensionalOres.MOD_ID, "nether_diamond"), new BlockItem(NETHER_DIAMOND, new FabricItemSettings().group(DimensionalOres.MAIN_TAB)));
+            Registry.register(Registry.ITEM, new Identifier(DimensionalOres.MOD_ID, "nether_emerald"), new BlockItem(NETHER_EMERALD, new FabricItemSettings().group(DimensionalOres.MAIN_TAB)));
+        }
+    }
 
 }
