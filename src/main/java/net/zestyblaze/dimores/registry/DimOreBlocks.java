2824e2ff1ff2b099bbd0f6c275e755e52136c0e7
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2022-06-21 16:41:14 +0100

    1.18.2 full port

--- src/main/java/net/zestyblaze/dimores/registry/DimOreBlocks.java
+++ src/main/java/net/zestyblaze/dimores/registry/DimOreBlocks.java
@@ -2,30 +2,33 @@ package net.zestyblaze.dimores.registry;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.registry.Registry;
 import net.zestyblaze.dimores.DimOres;
 
 public class DimOreBlocks {
-    public static final Block QUARTZ_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool());
-    public static final Block NETHER_COAL_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.NETHERRACK).requiresTool());
-    public static final Block NETHER_COPPER_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.NETHERRACK).requiresTool());
-    public static final Block NETHER_IRON_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.NETHERRACK).requiresTool());
-    public static final Block NETHER_LAPIS_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.NETHERRACK).requiresTool());
-    public static final Block NETHER_REDSTONE_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.NETHERRACK).requiresTool());
-    public static final Block NETHER_DIAMOND_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.NETHERRACK).requiresTool());
-    public static final Block NETHER_EMERALD_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.NETHERRACK).requiresTool());
-    public static final Block END_COAL_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool());
-    public static final Block END_COPPER_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool());
-    public static final Block END_IRON_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool());
-    public static final Block END_GOLD_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool());
-    public static final Block END_LAPIS_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool());
-    public static final Block END_REDSTONE_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool());
-    public static final Block END_DIAMOND_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool());
-    public static final Block END_EMERALD_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool());
-    public static final Block END_QUARTZ_ORE = new Block(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool());
+    public static final Block QUARTZ_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool(), UniformIntProvider.create(2, 5));
+    public static final Block NETHER_COAL_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.NETHERRACK).requiresTool(), UniformIntProvider.create(2, 6));
+    public static final Block NETHER_COPPER_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.NETHERRACK).requiresTool());
+    public static final Block NETHER_IRON_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.NETHERRACK).requiresTool());
+    public static final Block NETHER_LAPIS_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.NETHERRACK).requiresTool(), UniformIntProvider.create(3, 6));
+    public static final Block NETHER_REDSTONE_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.NETHERRACK).requiresTool(), UniformIntProvider.create(3, 6));
+    public static final Block NETHER_DIAMOND_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.NETHERRACK).requiresTool(), UniformIntProvider.create(4, 8));
+    public static final Block NETHER_EMERALD_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.NETHERRACK).requiresTool(), UniformIntProvider.create(4, 9));
+    public static final Block END_COAL_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool(), UniformIntProvider.create(3, 6));
+    public static final Block END_COPPER_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool());
+    public static final Block END_IRON_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool());
+    public static final Block END_GOLD_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool());
+    public static final Block END_LAPIS_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool(), UniformIntProvider.create(3, 7));
+    public static final Block END_REDSTONE_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool(), UniformIntProvider.create(3, 7));
+    public static final Block END_DIAMOND_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool(), UniformIntProvider.create(4, 9));
+    public static final Block END_EMERALD_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool(), UniformIntProvider.create(5, 9));
+    public static final Block END_QUARTZ_ORE = new OreBlock(FabricBlockSettings.of(Material.STONE).strength(3, 3).sounds(BlockSoundGroup.STONE).requiresTool(), UniformIntProvider.create(3, 6));
 
     public static void registerBlocks() {
         Registry.register(Registry.BLOCK, new Identifier(DimOres.MOD_ID, "quartz_ore"), QUARTZ_ORE);
